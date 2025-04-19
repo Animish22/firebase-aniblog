78bc19f1476fa58992cf2367fe7c11877ae1d086
animish <animishharitash@gmail.com>
2025-04-19 21:16:53 +0530

    Added tRPC server packages to frontend

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,12 +1,12 @@
 //this file is always rendered once an api request is made to the server so the context gets updated for each request . 
 
-import { initTRPC } from '@trpc/server'
+import { initTRPC} from '@trpc/server'
 import type { CreateExpressContextOptions } from '@trpc/server/adapters/express';
 
 export const createContext = ({ req, res }: CreateExpressContextOptions) => {
     return {
         req,
-        res,
+        res, 
     }
 }
 const t = initTRPC.context<typeof createContext>().create();
