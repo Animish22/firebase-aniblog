f371cded6999b242161e768e3555ac3948a163e3
animish <animishharitash@gmail.com>
2025-04-19 21:26:13 +0530

    Commented tRPC as not fireStore and not Mongodb now

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,8 @@
-import { router } from "./trpc";
+// import { router } from "./trpc";
 
-export const appRouter = router({
-    //Parse image using multer and then store it in cloudinary and then get the imageUrl from cloudinary 
+// export const appRouter = router({
+//     //Parse image using multer and then store it in cloudinary and then get the imageUrl from cloudinary 
 
-})
+// })
 
-export type AppRouter = typeof appRouter;
+// export type AppRouter = typeof appRouter;
--- server/trpc.ts
+++ server/trpc.ts
@@ -1,16 +1,16 @@
 //this file is always rendered once an api request is made to the server so the context gets updated for each request . 
 
-import { initTRPC} from '@trpc/server'
-import type { CreateExpressContextOptions } from '@trpc/server/adapters/express';
+// import { initTRPC} from '@trpc/server'
+// import type { CreateExpressContextOptions } from '@trpc/server/adapters/express';
 
-export const createContext = ({ req, res }: CreateExpressContextOptions) => {
-    return {
-        req,
-        res, 
-    }
-}
-const t = initTRPC.context<typeof createContext>().create();
+// export const createContext = ({ req, res }: CreateExpressContextOptions) => {
+//     return {
+//         req,
+//         res, 
+//     }
+// }
+// const t = initTRPC.context<typeof createContext>().create();
 
-export const router = t.router;
-export const procedure = t.procedure;
+// export const router = t.router;
+// export const procedure = t.procedure;
 
